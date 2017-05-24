c6e6f11570bb1ce7f5313eaeb88a1362b158ab29
Marco Napetti <marco.napetti@gmail.com>
2017-05-24 22:59:03 +0200

    Benchmarking

--- app/store/GroupedItems.js
+++ app/store/GroupedItems.js
@@ -2,35 +2,40 @@
 Ext.define('HomeAccounting.store.GroupedItems', {
 	extend: 'Ext.data.ChainedStore',
 
-    storeId: 'GroupedItems',
-    source: 'EventRows',
-    grouper: {
-        groupFn: function(oRecord) {
-            return oRecord.get('item').toLowerCase().trim();
-        }
-    },
-    listeners: {
-        datachanged: Ext.Function.createBuffered(function(oStore) {
+	storeId: 'GroupedItems',
+	source: 'EventRows',
+	grouper: {
+		groupFn: function(oRecord) {
+			return oRecord.get('item').toLowerCase().trim();
+		}
+	},
+	listeners: {
+		datachanged: Ext.Function.createBuffered(function(oStore) {
 			var oStart = new Date();
-            try {
-                var oItems = Ext.getStore('Items') || Ext.create('HomeAccounting.store.Items');
-                oItems.removeAll();
-                oStore.getGroups().each(function(oGroup) {
-                    oItems.add({
-                        name: oGroup.getGroupKey(),
-                        total: oGroup.sum('total'),
-                        count: oGroup.count(),
-                        min: oGroup.min('total'),
-                        max: oGroup.max('total')
-                    })
-                });
-            }
-            catch(e) {
-                if(console && console.log) {
-                    console.log(e);
-                }
-            }
-            console.log('HomeAccounting.store.GroupedItems.datachanged ' + Ext.Date.diff(oStart, new Date, Ext.Date.MILLI));
-        }, 100)
-    }
+			try {
+				var oItems = Ext.getStore('Items') || Ext.create('HomeAccounting.store.Items'),
+					aItems = [];
+
+				oItems.removeAll();
+
+				oStore.getGroups().each(function(oGroup) {
+					aItems.push({
+						name: oGroup.getGroupKey(),
+						total: oGroup.sum('total'),
+						count: oGroup.count(),
+						min: oGroup.min('total'),
+						max: oGroup.max('total')
+					});
+				});
+
+				oItems.add(aItems);
+			}
+			catch(e) {
+				if(console && console.log) {
+					console.log(e);
+				}
+			}
+			console.log('HomeAccounting.store.GroupedItems.datachanged ' + Ext.Date.diff(oStart, new Date, Ext.Date.MILLI));
+		}, 100)
+	}
 });
